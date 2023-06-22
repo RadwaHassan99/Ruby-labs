50cbee73c39f73ed5bea5e5ab28b1776e4f996f5
Radwa99 <radwa.prof.9@gmail.com>
2023-06-22 21:46:16 +0300

    Rails1,2

---
+++ Rails/lab3/app/controllers/books_controller.rb
@@ -0,0 +1,56 @@
+class BooksController < ApplicationController
+  http_basic_authenticate_with name: "admin", password: "admin"
+
+
+
+  def index
+    @books = Book.all
+  end
+  
+  def show
+    @books = Book.find(params[:id])
+  end
+
+  def new 
+    
+    @books = Book.new
+
+  end
+
+  def create 
+    @books = Book.new(product_params)
+    
+    if @books.save
+      redirect_to @book
+    else
+      @books.errors.full_messages
+    end  
+  end 
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    
+    if @book.update(product_params)
+      redirect_to @book
+    else
+      render :new, status: :unprocessable_entity 
+    end  
+  
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+
+    redirect_to root_path, status: :see_other
+  end  
+
+  private
+    def book_params
+      params.require(:book).permit(:name, :image, :price, :description,:user_id)
+    end
+end
